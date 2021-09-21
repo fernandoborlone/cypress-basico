7fd14065410ce8e9bb3f332de05e1ed9164ad660
Fernando Borlone <fernandoborlone@gmail.com>
2021-09-21 18:43:42 -0300

    feat(test): add more tests

--- cypress/integration/busca/busca.spec.js
+++ cypress/integration/busca/busca.spec.js
@@ -2,47 +2,79 @@
 
 const searchTerm = 'cypress.io'
 
-describe('Busca', () => {
+describe('Exemplos de busca com Cypress', () => {
 
-    beforeEach(() => {
-        cy.intercept(
-            'GET',
-            `**?q=${searchTerm}**`
-        ).as('getSearchResults')
-        cy.visit('https://duckduckgo.com/')
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      `**?q=${searchTerm}**`
+    ).as('getSearchResults')
+    cy.visit('https://duckduckgo.com/')
 
-        cy.get('input[type="text"]')
-          .as('searchField')
-        .should('be.visible')
-    })
+    cy.get('input[type="text"]')
+      .as('searchField')
+      .should('be.visible')
+  })
+
+  context('3 maneiras de testar funcionalidades de busca', () => {
 
     it('Digita e pressiona tecla Enter', () => {
-        cy.get('@searchField')
-          .type(`${searchTerm}{enter}`)
+      cy.get('@searchField')
+        .type(`${searchTerm}{enter}`)
 
-        cy.wait('@getSearchResults')
+      cy.wait('@getSearchResults')
 
-        //Assert
-        cy.VerificaResultados()        
+      //Assert
+      cy.verificaResultados()
     })
 
     it('Digita e seleciona o botão buscar', () => {
-        cy.get('@searchField')
-          .type(searchTerm)
-        cy.get('input[type="submit"')
-          .should('be.visible')
-          .click()
-        
-          //Assert
-        cy.VerificaResultados()
+      cy.get('@searchField')
+        .type(searchTerm)
+      cy.get('input[type="submit"')
+        .should('be.visible')
+        .click()
+
+      //Assert
+      cy.verificaResultados()
     })
 
     it('digita e submete o formulário diretamente', () => {
-        cy.get('@searchField')
-          .type(searchTerm)
-        cy.get('form').submit()
+      cy.get('@searchField')
+        .type(searchTerm)
+      cy.get('form').submit()
 
-        //Assert
-        cy.VerificaResultados()
+      //Assert
+      cy.verificaResultados()
     })
+  })
+
+  // Testar a funcionalidade de busca
+  // Selecionando itens através da lista de auto-complete
+  context('selecionar item do auto-complete de busca', () => {
+
+    it('Busca e seleciona o primeiro item da lista', () => {
+      cy.get('@searchField')
+        .type('cypress.io{downarrow}{enter}', { delay: 100 })
+
+      //Assert
+      cy.verificaResultados()
+    })
+
+    it('Busca e seleciona o segundo item da lista', () => {
+      cy.get('@searchField')
+        .type('cypress.io{downarrow}{downarrow}{enter}', { delay: 250 })
+
+      //Assert
+      cy.verificaResultados()
+    })
+
+    it('Busca e seleciona o quarto item da lista', () => {
+      cy.get('@searchField')
+        .type('cypress.io{downarrow}{downarrow}{downarrow}{downarrow}{enter}', { delay: 300 })
+
+      //Assert
+      cy.verificaResultados()
+    })
+  })
 })
