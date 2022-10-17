7b4dffcb679a3df1c34d1ef857210b349be8d7fa
Fernando Borlone <fernandoborlone@gmail.com>
2022-10-17 20:48:03 -0300

    atualização da imagem cypress dockerfile

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra')
 const path = require('path')
 
 function getConfigurationByFile(file) {
-    const pathToConfigFile = path.resolve('..', 'cypress-basico/cypress/config', `${file}.json`)
+    const pathToConfigFile = path.resolve('..', 'cypress-pipeline-example/cypress/config', `${file}.json`)
     console.log(pathToConfigFile)
 
     return fs.readJSON(pathToConfigFile)
